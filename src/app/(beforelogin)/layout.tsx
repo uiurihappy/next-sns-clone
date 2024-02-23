9a7a6208b748c4fff1947b35ed111789038f3566
ybchar <uiurihappy@sju.ac.kr>
2024-02-23 22:25:17 +0900

    feat: convert to client component

--- src/app/(beforelogin)/layout.tsx
+++ src/app/(beforelogin)/layout.tsx
@@ -3,7 +3,8 @@ import styles from "@/app/page.module.css";
 
 type Props = { children: ReactNode; modal: ReactNode };
 
-export default function Layout({ children, modal }: Props) {
+// 서버에서 돌아가기에 async 즉, 비동기로도 사용이 가능하다.
+export default async function Layout({ children, modal }: Props) {
   return (
     <div className={styles.container}>
       {children}
--- src/app/(beforelogin)/page.tsx
+++ src/app/(beforelogin)/page.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css"; // css module 사용
+import ybchaLogo from "../../../public/ybchaLogo.png";
 /* 많이 사용하는 거
 - tailwind -> 호불호 심함, 가독성이 떨어진대
 - Styled Component -> Server Component SSR
