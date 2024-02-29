fffec4d965e733c22ea022a89041d4196643aa01
ybchar <uiurihappy@sju.ac.kr>
2024-02-29 17:53:26 +0900

    fix: layout 수정

--- src/app/(beforelogin)/@modal/(.)i/flow/login/page.tsx
+++ src/app/(beforelogin)/@modal/(.)i/flow/login/page.tsx
@@ -8,7 +8,7 @@
 */
 // 클라이언트에서 라우팅할 때만 인터셉트 라우팅이 적용된다.
 
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 
 // 서버 컴포넌트는 클라이언트 컴포넌트를 import 해도 되지만, 반대로 클라이언트 컴포넌트가 서버 컴포넌트를 import 하면 안된다.
 export default function Login() {
@@ -20,6 +20,8 @@ export default function Login() {
    * localhost:3000 -> localhost:3000/login -> localhost:3000/i/flow/login
    * 이렇게 리다이렉트는 동일하나 뒤로 갔을 시 push는 login으로 바로 이전 페이지(/login)를 간다.
    * 그러나 replace는 한 단계 더 뒤인 / 페이지로 이동한다.
+   *
+   * 이동 후 히스토리를 지워버린다
    */
   router.replace("/i/flow/login");
   return null;
--- src/app/(beforelogin)/login/page.tsx
+++ src/app/(beforelogin)/login/page.tsx
@@ -1,6 +1,29 @@
-import { redirect } from "next/navigation";
+"use client";
+import Image from "next/image";
+import styles from "@/app/page.module.css"; // css module 사용
+import ybchaLogo from "../../../../public/ybchaLogo.png";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
 
 export default function Login() {
-  redirect("/i/flow/login");
+  const router = useRouter();
+  router.replace("/i/flow/login");
+  return (
+    <>
+      <div className={styles.left}>
+        <Image src={ybchaLogo} alt="logo" />
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h2>지금 가입하세요.</h2>
+        <Link href="/i/flow/signUp" className={styles.signUp}>
+          계정 만들기
+        </Link>
+        <h3>이미 트위터에 가입하셨나요?</h3>
+        <Link href="/login" className={styles.login}>
+          로그인
+        </Link>
+      </div>
+    </>
+  );
 }
-  
