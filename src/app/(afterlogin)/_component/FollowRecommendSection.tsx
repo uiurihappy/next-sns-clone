b822b74d759c958fbc1bb629d3067f8a1e79755b
ybchar <uiurihappy@sju.ac.kr>
2024-03-09 15:12:46 +0900

    feat: 오른쪽 섹션 구현

--- src/app/(afterlogin)/_component/FollowRecommendSection.tsx
+++ src/app/(afterlogin)/_component/FollowRecommendSection.tsx
@@ -1,3 +1,31 @@
-export default function FollowRecommendSection() {
-  return <div>추천</div>;
+'use client';
+
+// onClick? -> 클라이언트 컴포넌트로 전환
+import style from './followRecommendSection.module.css';
+
+export default function FollowRecommend() {
+  const onFollow = () => {};
+
+  const user = {
+    id: 'elonmusk',
+    nickname: 'Elon Musk',
+    image: '/yRsRRjGO.jpg',
+  };
+
+  return (
+    <div className={style.container}>
+      <div className={style.userLogoSection}>
+        <div className={style.userLogo}>
+          <img src={user.image} alt={user.id} />
+        </div>
+      </div>
+      <div className={style.userInfo}>
+        <div className={style.title}>{user.nickname}</div>
+        <div className={style.count}>@{user.id}</div>
+      </div>
+      <div className={style.followButtonSection}>
+        <button onClick={onFollow}>팔로우</button>
+      </div>
+    </div>
+  );
 }
--- src/app/(afterlogin)/_component/TrendSection.tsx
+++ src/app/(afterlogin)/_component/TrendSection.tsx
@@ -7,6 +7,15 @@ export default function TrendSection() {
       <div className={style.trend}>
         <h3>나를 위한 트렌드</h3>
         <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
+        <Trend />
       </div>
     </div>
   );
