e344e3de746802907215a89c4ec9d8e45b2e8f6e
ybchar <uiurihappy@sju.ac.kr>
2024-03-17 22:59:49 +0900

    feat: profile 페이지

--- src/app/(afterlogin)/[username]/page.tsx
+++ src/app/(afterlogin)/[username]/page.tsx
@@ -1,3 +1,39 @@
+import style from './profile.module.css';
+import BackButton from '@/app/(afterlogin)/_component/BackButton';
+import Post from '@/app/(afterlogin)/_component/Post';
+
 export default function Profile() {
-  return <main>프로필</main>;
+  
+  const user = {
+    id: 'ybcha',
+    nickname: 'Charlie',
+    image: '/ybchaLogo.png',
+  };
+
+  return (
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{user.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={user.image} alt={user.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{user.nickname}</div>
+          <div>@{user.id}</div>
+        </div>
+        <button className={style.followButton}>팔로우</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </main>
+  );
 }
