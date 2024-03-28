d45965e5093b213ed9ad5fe45a87f4090f0b0766
ybchar <uiurihappy@sju.ac.kr>
2024-03-28 14:21:15 +0900

    comment: message 컴포넌트 주석

--- src/app/(afterlogin)/messages/_component/Room.tsx
+++ src/app/(afterlogin)/messages/_component/Room.tsx
@@ -15,6 +15,9 @@ export default function Room() {
   const user = {
     id: 'hero',
     nickname: '영웅',
+    /* 대문자로 하는 이유
+     서버 측에서 join한 컬럼에 대해서는 대문자로 오기 때문에 대문자로 처리 (prisma에서 그렇다고 함 ㅇㅇ)
+    */
     Messages: [
       { roomId: 123, content: '안녕하세요.', createdAt: dayjs().format('YYYY-MM-DD HH:mm:ss') },
       { roomId: 123, content: '안녕히가세요.', createdAt: dayjs().format('YYYY-MM-DD HH:mm:ss') },
@@ -32,13 +35,18 @@ export default function Room() {
       </div>
       <div className={style.roomChatInfo}>
         <div className={style.roomUserInfo}>
+          {/* bold */}
           <b>{user.nickname}</b>
+          {/* Non-breaking Space, 줄바꿈을 일으키지 않는 공백 */}
           &nbsp;
           <span>@{user.id}</span>
           &nbsp; · &nbsp;
           <span className={style.postDate}>{dayjs(user.Messages?.at(-1)?.createdAt).fromNow(true)}</span>
         </div>
-        <div className={style.roomLastChat}>{user.Messages?.at(-1)?.content}</div>
+        <div className={style.roomLastChat}>
+          {/* 마지막 index를 가져오기 용이하다. */}
+          {user.Messages?.at(-1)?.content}
+        </div>
       </div>
     </div>
   );
